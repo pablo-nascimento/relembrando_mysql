fa72f2bb369ac0a43e08e8a02f7efbb46bb1c307
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-04-02 20:03:51 -0300

    Comandos para criar o BD e criar as tabelas com os seus atributos.

--- Criando_Tabelas.sql
+++ Criando_Tabelas.sql
@@ -2,4 +2,15 @@
 # Digital Innovation One
 # 02.04.2022 - 19h45
 
+# Criando o BD para estudo.
+CREATE DATABASE ESTUDO;
+
+# Selecionando o BD criado acima para uso.
+USE ESTUDO;
+
 # Criando Tabelas
+CREATE TABLE pessoa
+(
+nome VARCHAR(20),
+nascimento DATE
+)
