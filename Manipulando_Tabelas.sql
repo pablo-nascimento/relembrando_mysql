71bd820c5ee1f32d3c0e371190ee9110baab580d
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-04-02 20:58:13 -0300

    Inserindo mais dados na tabela. SELECT com apenas alguns dos campos.

--- Manipulando_Tabelas.sql
+++ Manipulando_Tabelas.sql
@@ -6,6 +6,17 @@
 # Selecionando o BD para uso.
 USE ESTUDO;
 
+# Inserindo mais dados para posterior manipulação
+INSERT INTO pessoa(nome,nascimento) VALUES ('Teste_01','2022-03-31');
+INSERT INTO pessoa(nome,nascimento) VALUES ('Teste_02','2022-04-30');
+INSERT INTO pessoa(nome,nascimento) VALUES ('Teste_003','2022-04-01');
+
 # Trabalhando com SELECT
 # SELECT * FROM nome_tabela
 SELECT * FROM pessoa;
+
+# SELECT com apenas um dos campos.
+# SELECT nome_campo FROM nome_tabela.
+SELECT nome FROM pessoa;
+SELECT id, nome FROM pessoa;
+SELECT nome, nascimento FROM pessoa;
