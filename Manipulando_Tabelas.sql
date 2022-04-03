345452108328791dfd485334ba4b7f7df0405fa7
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-04-03 10:55:32 -0300

    Utilizando o ALTER TABLE para adicionar um novo campo

--- Manipulando_Tabelas.sql
+++ Manipulando_Tabelas.sql
@@ -9,7 +9,7 @@ USE ESTUDO;
 # Inserindo mais dados para posterior manipulação
 INSERT INTO pessoa(nome,nascimento) VALUES ('Teste_01','2022-03-31');
 INSERT INTO pessoa(nome,nascimento) VALUES ('Teste_02','2022-04-30');
-INSERT INTO pessoa(nome,nascimento) VALUES ('AB','2022-04-03');
+INSERT INTO pessoa(nome,nascimento,genero) VALUES ('ABC','2022-04-03','M');
 
 # Trabalhando com SELECT
 # SELECT * FROM nome_tabela
@@ -24,7 +24,7 @@ SELECT nome, nascimento FROM pessoa;
 # UPDATE - Atualizando informações na tabela
 # clausula WHERE - usada, para neste caso, indicar o campo onde se quer atualizar.
 # UPDATE nome_tabela SET nome_campo = 'dado_para_atualizar' WHERE id = 1
-UPDATE pessoa SET nome = 'Pablito' WHERE id = 2;
+UPDATE pessoa SET genero = 'F' WHERE id = 6;
 
 # DELETE - Apagar dados. Uso semelhante ao do comando acima.
 # DELETE from nome_tabela WHERE id = 1;
@@ -35,6 +35,16 @@ UPDATE pessoa SET nome = 'Pablito' WHERE id = 2;
 # DESC - Clausula usada para ordenação de forma decrescente
 SELECT * FROM pessoa ORDER BY nome;
 SELECT * FROM pessoa ORDER BY nascimento DESC;
+
+
+# Agrupamento - contar quantas informacoes atendem a um determinado critério.
+
+
+# Clausula ALTER TABLE - Alteracoes em tabelas já criadas.
+# ALTER TABLE nome_tabela O_QUE SE QUER FAZER (ADD) nome_campo TIPO_DE_DADO(TAMANHO)
+# NOT NULL ONDE_SE_QUER_COLOCAR_O_CAMPO(AFTER OU BEFORE, por exemplo) nome_campo
+ALTER TABLE pessoa ADD genero VARCHAR(1) AFTER nascimento;
+
 
 
 
