c3fd22bb1a0fa3a2520f5c400f2acd9114afb8b1
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-04-05 20:47:51 -0300

    Lidando com Relacionamentos entre tabelas.

---
+++ Relacionamentos.sql
@@ -0,0 +1,9 @@
+# Curso MySQL - Explorando os relacionamentos com o Workbench. 
+# Lidando com os relacionamentos
+# Digital Innovation One
+# 05.04.2022 - 20h46
+
+USE ESTUDO;
+
+# Inner join - leitura de multiplas tabelas.
+
