0f5487a5251706f8ba90010398b43b467398526c
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-04-05 20:52:05 -0300

    Select usado no INNER JOIN.

--- Relacionamentos.sql
+++ Relacionamentos.sql
@@ -6,4 +6,7 @@
 USE ESTUDO;
 
 # Inner join - leitura de multiplas tabelas.
+# SELECT com INNER JOIN
+# SELECT * from tabela_1 JOIN tabela_2 ON tabela_1.chave_estrangeira = tabela_2.chave_estrangeira
+
 
