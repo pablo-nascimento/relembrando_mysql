daa3481089184619308fe568dc5a0180f503cff0
Pablo Nascimento <pablonascimentoaraujo@hotmail.com>
2022-04-18 20:01:46 -0300

    Adicionando novas tabelas e inserindo dados.

--- Consultas_com_Join.sql
+++ Consultas_com_Join.sql
@@ -6,3 +6,30 @@
 CREATE DATABASE CONSULTA;
 
 USE CONSULTA;
+
+CREATE TABLE canais
+(
+id_canal INT(11) NOT NULL,
+nome_canal VARCHAR(100) NOT NULL
+);
+
+INSERT INTO `canais` (`id_canal`, `nome_canal`) VALUES
+(1, 'React'),
+(2, 'PHP'),
+(3, 'CSS'),
+(4, 'HTML');
+
+CREATE TABLE videos
+(
+id_video INT(11) NOT NULL,
+nome_video VARCHAR(100) NOT NULL,
+autor_video VARCHAR(100) NOT NULL
+);
+
+INSERT INTO `videos` (`id_video`, `nome_video`, `autor_video`) VALUES
+(1, 'Login com React', 'React'),
+(2, 'Componentes com React', 'React'),
+(3, 'Listas com PHP', 'PHP'),
+(4, 'Funções com PHP', 'PHP'),
+(5, 'Páginas com HTML', 'HTML');
+
